0eb172cee68e908f78ceb2d6750bb3bd50335e72
HYI110100 <53818512+HYI110100@users.noreply.github.com>
2024-12-14 19:02:47 +0800

    fix: router name repeat

--- packages/frontend/src/router/index.ts
+++ packages/frontend/src/router/index.ts
@@ -29,7 +29,7 @@ const router = createRouter({
         },
         {
           path: "/test",
-          name: "Send",
+          name: "Test",
           component: () => import("~/pages/test.vue"),
         },
       ],
